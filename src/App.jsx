1b816aab9649743fddc1c6562e590d4808824ab3
KhunnDev <bankbav3@gmail.com>
2024-09-25 10:27:05 +0700

    '

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ const App = () => {
   useEffect(() => {
     const checkHealth = async () => {
       try {
-        const response = await fetch('http://localhost:3334/health');
+        const response = await fetch('http://backend:3334/health');
         if (response.ok) {
           toast.success("เซิร์ฟเวอร์ทำงานปกติ!");
         } else {
